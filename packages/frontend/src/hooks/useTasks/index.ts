e86e29c5d9a984c3481d7747fabad5ba391eff98
lvil <mysecretemail@gmail.com>
2025-08-27 20:05:16 +0300

    Moved all TaskStoreFunction to store.ts

--- packages/frontend/src/hooks/useTasks/index.ts
+++ packages/frontend/src/hooks/useTasks/index.ts
@@ -4,56 +4,30 @@ import { useTaskStore } from "@stores/task-store/taskStore";
 import { ITask } from "@services/task-service/types";
 import { IUseTasks } from "@hooks/useTasks/types";
 import {
-	handleTaskDelete,
 	handleTaskCreate,
-	handleTaskUpdate,
+	handleTaskDelete,
 	handleTaskStatusUpdate,
+	handleTaskUpdate,
 } from "@hooks/useTasks/consts";
-import { deleteTask } from "@services/task-service/consts";
+import { createStoreHandlers } from "@hooks/useTasks/store";
 
 export const useTasks = (): IUseTasks => {
-	const {
-		getTaskById,
-		setEditedTask,
-		setTasks,
-		addTask,
-		updateTask,
-		deleteTask,
-	} = useTaskStore();
+	const taskStore = useTaskStore();
 
 	useEffect(() => {
 		taskService.getTasks().then((tasks) => {
-			setTasks(tasks);
+			taskStore.setTasks(tasks);
 		});
 	}, []);
 
-	const onSaveSuccess = (TaskId: ITask["id"], task: ITask) => {
-		updateTask(TaskId, { ...task, isPending: false });
-		setEditedTask(null);
-	};
-
-	const onPreSave = (task: ITask) => {
-		addTask({ ...task, isPending: true });
-	};
-
-	const onPreUpdate = (task: ITask) => {
-		updateTask(task.id, { isPending: true });
-	};
-
-	const onPreDelete = (TaskId: ITask["id"]) => {
-		updateTask(TaskId, { isPending: true });
-	};
-
-	const onDeleteSuccess = (TaskId: ITask["id"]) => {
-		deleteTask(TaskId);
-	};
-
-	const onEditTask = (id: ITask["id"]) => {
-		const task = getTaskById(id);
-		setEditedTask(task);
-	};
-
-	// TODO: add error handling
+	const {
+		onSaveSuccess,
+		onPreUpdate,
+		onPreSave,
+		onDeleteSuccess,
+		onPreDelete,
+		onEditTask,
+	} = createStoreHandlers(taskStore);
 
 	return {
 		saveTask: (task) => {
